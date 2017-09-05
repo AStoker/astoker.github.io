874cc6156c33592d085ee48c1dd55a8d01acd32e
Andrew Stoker <TheAndyBob@me.com>
2017-09-05 17:11:06 -0400

    Some work to get ES6 only site working

--- aurelia_project/tasks/run.js
+++ aurelia_project/tasks/run.js
@@ -1,54 +1,66 @@
 import gulp from 'gulp';
 import browserSync from 'browser-sync';
 import historyApiFallback from 'connect-history-api-fallback/lib';
-import {CLIOptions} from 'aurelia-cli';
+import { CLIOptions } from 'aurelia-cli';
 import project from '../aurelia.json';
 import build from './build';
 import watch from './watch';
 
-let serve = gulp.series(
-  build,
-  done => {
+const runAsES6 = CLIOptions.hasFlag('es6');
+
+function startBrowser(done) {
     browserSync({
-      online: false,
-      open: false,
-      port: 9000,
-      logLevel: 'silent',
-      server: {
-        baseDir: [project.platform.baseDir],
-        middleware: [historyApiFallback(), function(req, res, next) {
-          res.setHeader('Access-Control-Allow-Origin', '*');
-          next();
-        }]
-      }
-    }, function (err, bs) {
-      if (err) return done(err);
-      let urls = bs.options.get('urls').toJS();
-      log(`Application Available At: ${urls.local}`);
-      log(`BrowserSync Available At: ${urls.ui}`);
-      done();
+        online: false,
+        open: false,
+        port: 9000,
+        logLevel: 'verbose',
+        server: {
+            baseDir: [project.platform.baseDir],
+            index: 'index.es6.html', //runAsES6 ? 'index.es6.html' : 'index.html',
+            middleware: [historyApiFallback(), function(req, res, next) {
+                res.setHeader('Access-Control-Allow-Origin', '*');
+                next();
+            }]
+        }
+    }, function(err, bs) {
+        if (err) return done(err);
+        let urls = bs.options.get('urls').toJS();
+        log(`Application Available At: ${urls.local}`);
+        log(`BrowserSync Available At: ${urls.ui}`);
+        done();
     });
-  }
+}
+
+let buildNServe = gulp.series(
+    build,
+    startBrowser
 );
+let serveES6 = gulp.series(startBrowser);
 
 function log(message) {
-  console.log(message); //eslint-disable-line no-console
+    console.log(message); //eslint-disable-line no-console
 }
 
 function reload() {
-  log('Refreshing the browser');
-  browserSync.reload();
+    log('Refreshing the browser');
+    browserSync.reload();
 }
 
 let run;
-
+let serve = buildNServe;
+if (runAsES6) {
+    serve = serveES6;
+}
 if (CLIOptions.hasFlag('watch')) {
-  run = gulp.series(
-    serve,
-    done => { watch(reload); done(); }
-  );
+    run = gulp.series(
+        serve,
+        done => {
+            watch(reload);
+            done();
+        }
+    );
 } else {
-  run = serve;
+    run = serve;
 }
 
 export default run;
