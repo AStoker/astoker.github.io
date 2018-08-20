fc4bb17248e8ecd3234c6c0decc64f25860b5414
Andrew Stoker <Andy.Stoker@iCloud.com>
2018-08-20 09:33:53 -0400

    little logs

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ export function configure(aurelia) {
     .standardConfiguration()
     .feature('resources');
 
+  logWelcomeMessage();
+
   if (environment.debug) {
     aurelia.use.developmentLogging();
   }
@@ -14,4 +16,12 @@ export function configure(aurelia) {
   }
 
   aurelia.start().then(() => aurelia.setRoot());
+}
+
+function logWelcomeMessage() {
+  console.log('Curious one, aren\'t you?');
+  console.log('"Under Construction" is a very loose term right now.');
+  console.log('Hit me up over on GitHub (https://github.com/AStoker) if you need anything.');
+  console.log('Party on dudes!');
+  console.log('https://youtu.be/rph_1DODXDU');
 }
